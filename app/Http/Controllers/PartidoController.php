170e252e63eb63254ab8e2823e460fd544ab5a23
franciscocamposchimal <camposfranciscoing@gmail.com>
2018-03-16 18:48:45 -0600

    fix partido

--- app/Http/Controllers/PartidoController.php
+++ app/Http/Controllers/PartidoController.php
@@ -26,15 +26,8 @@ class PartidoController extends Controller
 
     public function putPartido(Request $request, $id_partido)
     {
-        /*$partido = FasesDetalle::with(['fase' => function($query){
-            $query->select(['home', 'visit', 'empate']);
-        }])->where('id', 1);*/
-        $partido = FasesDetalle::select('home', 'visit', 'empate')->where('id', $id_partido)->get();
+        $partido = Partido::with(['faseDetalle','equipoHome','equipoVisit'])->where('id', $id_partido)->get();
 
-        $grupo_detalle = FasesDetalle::with(['partido.equipoVisit' => function ($query){
-            $query->select(['pj', 'pg', 'e', 'pp', 'gf', 'gc', 'pts']);
-        }])->get();
-
-        return response()->json(['partido'=>$partido, 'grupo'=>$grupo_detalle],200);
+        return response()->json(['partido'=>$partido],200);
     }
 }
