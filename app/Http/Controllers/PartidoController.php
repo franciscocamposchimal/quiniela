a372e010f4f686e51b29d44ffcc5d632441755b1
MikeX <miguel_bsd@hotmail.com>
2018-03-20 17:52:05 -0600

    Auth user info added

--- app/Http/Controllers/PartidoController.php
+++ app/Http/Controllers/PartidoController.php
@@ -9,6 +9,7 @@ use App\FasesDetalle;
 use App\Partido;
 use App\GpoDetalle;
 use App\Quinela;
+use App\User;
 
 class PartidoController extends Controller
 {
@@ -32,6 +33,9 @@ class PartidoController extends Controller
 
     public function putPartido(Request $request, $id_partido)
     {
+        $user = User::where('role', 1)->get(['role']);
+        /*if($user->role == 1)
+        {
         $partido = Partido::find($id_partido);
         $updateHome= GpoDetalle::where('id',$partido->equipoHome->id)->first();
         $updateVisit= GpoDetalle::where('id',$partido->equipoVisit->id)->first();
@@ -114,7 +118,10 @@ class PartidoController extends Controller
                     $quinela->save();
                 }
             }
-
-        return response()->json(['partido'=>$Updatepartido],200);
+            return response()->json(['partido'=>$Updatepartido],200);
+        }else{
+            return response()->json(['error'=>'Unathorized'],200);
+        }*/
+        return response()->json(['partido'=>$user],200);
     }
 }
